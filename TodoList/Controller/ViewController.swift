1a498039a0a41ebf5946eae7290bf2c4b45cd000
henryLu <henryxlu220@gmail.com>
2019-08-08 15:54:11 +0800

    keyboard retune

--- TodoList/Controller/ViewController.swift
+++ TodoList/Controller/ViewController.swift
@@ -18,21 +18,25 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
             self.moveView.backgroundColor = #colorLiteral(red: 0.1411764771, green: 0.3960784376, blue: 0.5647059083, alpha: 1)
         }
     }
+    @IBOutlet weak var moveViewBottomConstraint: NSLayoutConstraint!
+    
+    
     
     var lists :Array = [List]()
     
-    @IBOutlet weak var moveViewBottomConstraint: NSLayoutConstraint!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.tableView.delegate = self
         self.tableView.dataSource = self
+        self.textField.delegate = self
         
         let nib = UINib(nibName: "ListTableViewCell", bundle: nil)
         tableView.register(nib, forCellReuseIdentifier: "listCell")
         
         NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
     
     }
     
@@ -45,7 +49,6 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         //        guard let input = textField.text else { return }
         if textField.text != "" {
             addList()
-            
         }
         textField.text = ""
     }
@@ -55,23 +58,45 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         self.keyboardControl(notification, isShowing: true)
     }
     
+    
+    @objc func keyboardWillHide(notification: Notification) {
+        if let keyboardFrame: NSValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue {
+            let keyboardRectangle = keyboardFrame.cgRectValue
+            let keyboardHeight = keyboardRectangle.height
+            moveViewBottomConstraint.constant -= keyboardHeight - safeBottom
+            
+            //view 收起動畫
+            var userInfo = notification.userInfo!
+            let curve = (userInfo[UIResponder.keyboardAnimationCurveUserInfoKey]! as AnyObject).uint32Value
+            let options = UIView.AnimationOptions(rawValue: UInt(curve!) << 16 | UIView.AnimationOptions.beginFromCurrentState.rawValue)
+            let duration = (userInfo[UIResponder.keyboardAnimationDurationUserInfoKey]! as AnyObject).doubleValue
+            
+            UIView.animate(withDuration: duration!,delay: 0,options: options,animations: {
+                self.view.layoutIfNeeded()
+            },
+                           completion: { bool in
+            })
+            
+        }
+    }
+    
+    var safeBottom:CGFloat = 0
+    
     private func keyboardControl(_ notification: Notification, isShowing: Bool) {
         
-        
-        /* Handle the Keyboard property of Default*/
-        
-        //keyboard height
+        //get keyboard height
         var userInfo = notification.userInfo!
         let keyboardRect = (userInfo[UIResponder.keyboardFrameEndUserInfoKey]! as AnyObject).cgRectValue
         let convertedFrame = self.view.convert(keyboardRect!, from: nil)
         let heightOffset = self.view.bounds.size.height - convertedFrame.origin.y
-        
+
 
         var  pureheightOffset : CGFloat = -heightOffset
         
         if isShowing { /// Wite space of save area in iphonex ios 11
             if #available(iOS 11.0, *) {
-                pureheightOffset = pureheightOffset + view.safeAreaInsets.bottom
+                safeBottom = view.safeAreaInsets.bottom
+                pureheightOffset = pureheightOffset + safeBottom
                 moveViewBottomConstraint.constant = -pureheightOffset
             }
         }
@@ -104,33 +129,13 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
 }
 
-//extension ViewController: UITextFieldDelegate {
-//
-//
-//
-//    //view隨鍵盤彈起
-//    func textFieldDidBeginEditing(_ textField: UITextField) {
-//        UIView.animate(withDuration: 0.1) {
-//            self.view.frame.origin.y = -150
-//        }
-//    }
-//
-//
-//    //編輯結束收起鍵盤
-////    func textFieldDidEndEditing(_ textField: UITextField) {
-////        UIView.animate(withDuration: 0.1) {
-////            self.view.frame.origin.y = 0
-////        }
-////    }
-//
-//    func textFieldShouldReturn(textField: UITextField) -> Bool {
-//
-//        textField.resignFirstResponder()
-//        //or
-//        //self.view.endEditing(true)
-//        return true
-//    }
-//
-//
-//}
+
+extension ViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+//        keyboardWillHide(notification: Notification)
+        return true
+    }
+
+}
 
